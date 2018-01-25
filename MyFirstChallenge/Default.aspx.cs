14971cfb5c4f899d4fd9f74f31df5000d0c6991e
Young Kim <younghyun87@gmail.com>
2018-01-25 14:58:19 -0500

    C# first challenge

---
+++ MyFirstChallenge/Default.aspx.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace MyFirstChallenge
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void okButton_Click(object sender, EventArgs e)
+        {
+            string age = ageTextBox.Text;
+            string money = moneyTextBox.Text;
+
+            string result = String.Format("At {0} years old, I would have expected you to have more than {1} in your pocket.", age, money);
+
+            resultLabel.Text = result;
+        }
+    }
+}
